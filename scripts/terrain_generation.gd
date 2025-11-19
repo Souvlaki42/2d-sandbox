f1228d5d17ea5bc3e772e2daf05055d5f3cd4a80
Souvlaki42 <ilias@moulas.dev>
2025-11-19 21:28:14 +0200

    feat: made terrain generation asynchronous

--- scripts/terrain_generation.gd
+++ scripts/terrain_generation.gd
@@ -1,50 +1,60 @@
 extends Node2D
 class_name TerrainGenerator
 
-var noise: FastNoiseLite = FastNoiseLite.new()
+var terrain_noise: FastNoiseLite = FastNoiseLite.new()
+var cave_noise: FastNoiseLite = FastNoiseLite.new()
 
 @export_category("Generation")
-@export var worldSize: int = 100
-@export var heightAddition: int = 25
-@export var terrainFrequency: float = 0.04
-@export var caveFrequency: float = 0.08
-@export var surfaceValue: float = 0.25
-@export var heightMultiplier: float = 25
-@export var seedNum: float = 0
+@export var world_size: int = 100
+@export var height_addition: int = 25
+@export var terrain_frequency: float = 0.04
+@export var cave_frequency: float = 0.08
+@export var surface_value: float = 0.25
+@export var height_multiplier: float = 25
+@export var seed_num: float = 0
 @export var ground_offset: float = 600.0
 
 @export_category("Nodes")
 @export var tile: Sprite2D
-@export var noise_texture: ImageTexture
-
-var noise_image: Image
+@export var noise_texture: NoiseTexture2D
 
 func _ready() -> void:
-	seedNum = randi_range(-10000, 10000)
-	noise.seed = 0
-	noise.frequency = 1
-	noise.noise_type = FastNoiseLite.TYPE_PERLIN
-	noise.fractal_type = FastNoiseLite.FRACTAL_NONE
-	generate_noise_texture()
+	seed_num = randi_range(-10000, 10000)
+	
+	cave_noise.seed = 0
+	cave_noise.frequency = cave_frequency
+	cave_noise.offset = Vector3(seed_num, seed_num, 0)
+	cave_noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	cave_noise.fractal_type = FastNoiseLite.FRACTAL_NONE
+	
+	terrain_noise.seed = 0
+	terrain_noise.frequency = terrain_frequency
+	terrain_noise.offset = Vector3(seed_num, seed_num, 0)
+	terrain_noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	terrain_noise.fractal_type = FastNoiseLite.FRACTAL_NONE
+	
+	await generate_noise_texture()
 	generate_terrain()
 	
-func perlin_noise(x: float, y: float, frequency: float) -> float:
-	return (noise.get_noise_2d((x + seedNum) * frequency, (y + seedNum) * frequency) + 1.0) / 2.0
-	
 func generate_noise_texture() -> void:
-	noise_image = Image.create(worldSize, worldSize, true, Image.FORMAT_L8)
-	for x: int in range(noise_image.get_width()):
-		for y: int in range(noise_image.get_height()):
-			var v: float = perlin_noise(x, y, caveFrequency)
-			noise_image.set_pixel(x, y, Color(v, v, v))
-	noise_texture = ImageTexture.create_from_image(noise_image)
+	var gradient = Gradient.new()
+	gradient.set_color(0, Color.BLACK)
+	gradient.set_color(1, Color.WHITE)
+	noise_texture = NoiseTexture2D.new()
+	noise_texture.width = world_size
+	noise_texture.height = world_size
+	noise_texture.noise = cave_noise
+	noise_texture.color_ramp = gradient
+	noise_texture.normalize = true
+	await noise_texture.changed
 			
 func generate_terrain() -> void:
 	var tile_size: int = tile.texture.get_width()
-	for x: int in range(worldSize):
-		var height: float = perlin_noise(x, 0, terrainFrequency) * heightMultiplier + heightAddition
+	var noise_image: Image = noise_texture.get_image()
+	for x: int in range(world_size):
+		var height: float = terrain_noise.get_noise_2d(x, 0) * height_multiplier + height_addition
 		for y: int in range(height):
-			if noise_image.get_pixel(x, y).r > surfaceValue:
+			if noise_image.get_pixel(x, y).r > surface_value:
 				var new_tile: Node = tile.duplicate()
 				new_tile.visible = true
 				new_tile.position = Vector2(x * tile_size, ground_offset - (y * tile_size))
