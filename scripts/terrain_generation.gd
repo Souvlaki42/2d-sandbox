4094577478671b51efcf3c391eb144d981016bba
Souvlaki42 <ilias@moulas.dev>
2025-11-30 22:12:31 +0200

    feat: simplifying terrain generation

--- scripts/terrain_generation.gd
+++ scripts/terrain_generation.gd
@@ -65,25 +65,9 @@ func start_generation() -> void:
 	create_chunks()
 	generate_terrain()
 
-func get_biome(x: int, y: int) -> Biome:
-	return biome_lookup.get(biome_map.get_pixel(x, y), null)
-
-func create_chunks() -> void:
-	# TODO: Fix out of bounds errors
-	var num_of_chunks: int = roundi(float(world_size) / chunk_size)
-	world_chunks.resize(num_of_chunks)
-
-	for i: int in range(num_of_chunks):
-		var new_chunk: Node2D = Node2D.new()
-		new_chunk.name = "Chunk #%d" % i
-		world_chunks[i] = new_chunk
-		add_child(new_chunk)
-		if Engine.is_editor_hint():
-			new_chunk.owner = get_tree().edited_scene_root
-
 func draw_noise_images() -> void:
 	biome_noise = PerlinNoise.new(noise_seed, biome_frequency)
-	biome_map = generate_biome_image(biome_noise, world_size)
+	biome_map = generate_noise_image(biome_noise, world_size)
 
 	for biome in biomes:
 		biome.terrain_noise = PerlinNoise.new(noise_seed, biome.terrain_frequency)
@@ -95,6 +79,67 @@ func draw_noise_images() -> void:
 			ore.spread_image = generate_noise_image(ore.noise, world_size, ore.vein_size)
 
 	notify_property_list_changed()
+
+func create_chunks() -> void:
+	var num_of_chunks: int = roundi(world_size / chunk_size)
+	world_chunks.resize(num_of_chunks)
+
+	for i: int in range(num_of_chunks):
+		var new_chunk: Node2D = Node2D.new()
+		new_chunk.name = "Chunk #%d" % i
+		world_chunks[i] = new_chunk
+		add_child(new_chunk)
+		if Engine.is_editor_hint():
+			new_chunk.owner = get_tree().edited_scene_root
+
+func get_biome(x: int, y: int) -> Biome:
+	return biome_lookup.get(biome_map.get_pixel(x, y), null)
+
+func generate_terrain() -> void:
+	for x: int in range(world_size):
+		for y: int in range(world_size):
+			var current_biome: Biome = get_biome(x, y)
+			var height: float = current_biome.terrain_noise.get_unity_noise(x, 0) * current_biome.height_multiplier + height_addition
+			if y >= height: break
+			var current_tile: Tile = current_biome.tile_atlas.stone
+			if y < height - current_biome.dirt_layer_height and not current_biome.tile_atlas.get_ores().is_empty():
+				if current_biome.tile_atlas.coal.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.coal.max_spawn_height:
+					current_tile = current_biome.tile_atlas.coal
+				if current_biome.tile_atlas.iron.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.iron.max_spawn_height:
+					current_tile = current_biome.tile_atlas.iron
+				if current_biome.tile_atlas.gold.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.gold.max_spawn_height:
+					current_tile = current_biome.tile_atlas.gold
+				if current_biome.tile_atlas.diamond.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.diamond.max_spawn_height:
+					current_tile = current_biome.tile_atlas.diamond
+			elif y < int(height - 1):
+				current_tile = current_biome.tile_atlas.dirt
+			else:
+				current_tile = current_biome.tile_atlas.grass
+
+			if current_biome.cave_noise_image.get_pixel(x, y).r > 0.5 or not current_biome.generate_caves:
+				place_tile(current_tile, x, y)
+
+			if y > int(height - 1):
+				if randi_range(0, current_biome.tree_percent_chance) == 1 and world_tiles.has(Vector2i(x, y)):
+					place_tree(current_biome, x, y)
+				elif current_biome.tile_atlas.addons != null and randi_range(0, current_biome.addon_percent_chance) == 1 and world_tiles.has(Vector2i(x, y)):
+					place_tile(current_biome.tile_atlas.addons, x, y + 1)
+
+func generate_noise_image(noise: PerlinNoise, size: int, threshold: float = -1) -> Image:
+	var image: Image = Image.create_empty(size, size, true, Image.FORMAT_RGBA8)
+	image.fill(Color.BLACK)
+
+	for x: int in range(size):
+		for y: int in range(size):
+			var color = Color.BLACK
+			if threshold == -1:
+				color = biome_colors.sample(noise.get_unity_noise(x, y))
+			else:
+				color = Color.WHITE
+
+			if threshold == -1 or noise.get_unity_noise(x, y) > threshold:
+				image.set_pixel(x, y, color)
+	return image
 
 func place_tile(tile: Tile, x: int, y: int) -> void:
 	if world_tiles.has(Vector2i(x, y)): return
@@ -126,53 +171,3 @@ func place_tree(current_biome: Biome, x: int, y: int) -> void:
 
 		place_tile(current_biome.tile_atlas.tree_leaves, x + 1, y + tree_height + 1)
 		place_tile(current_biome.tile_atlas.tree_leaves, x + 1, y + tree_height + 2)
-
-func generate_noise_image(noise: PerlinNoise, size: int, threshold: float) -> Image:
-	var image: Image = Image.create_empty(size, size, true, Image.FORMAT_RGBA8)
-	image.fill(Color.BLACK)
-
-	for x: int in range(size):
-		for y: int in range(size):
-			if  noise.get_unity_noise(x, y) > threshold:
-				image.set_pixel(x, y, Color.WHITE)
-	return image
-
-func generate_biome_image(noise: PerlinNoise, size: int) -> Image:
-	var image: Image = Image.create_empty(size, size, true, Image.FORMAT_RGBA8)
-	image.fill(Color.BLACK)
-
-	for x: int in range(size):
-		for y: int in range(size):
-			var biome_color: Color = biome_colors.sample(noise.get_unity_noise(x, y))
-			image.set_pixel(x, y, biome_color)
-	return image
-
-func generate_terrain() -> void:
-	for x: int in range(world_size):
-		for y: int in range(world_size):
-			var current_biome: Biome = get_biome(x, y)
-			var height: float = current_biome.terrain_noise.get_unity_noise(x, 0) * current_biome.height_multiplier + height_addition
-			if y >= height: break
-			var current_tile: Tile = current_biome.tile_atlas.stone
-			if y < height - current_biome.dirt_layer_height and not current_biome.tile_atlas.get_ores().is_empty():
-				if current_biome.tile_atlas.coal.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.coal.max_spawn_height:
-					current_tile = current_biome.tile_atlas.coal
-				if current_biome.tile_atlas.iron.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.iron.max_spawn_height:
-					current_tile = current_biome.tile_atlas.iron
-				if current_biome.tile_atlas.gold.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.gold.max_spawn_height:
-					current_tile = current_biome.tile_atlas.gold
-				if current_biome.tile_atlas.diamond.spread_image.get_pixel(x, y).r > 0.5 and height - y > current_biome.tile_atlas.diamond.max_spawn_height:
-					current_tile = current_biome.tile_atlas.diamond
-			elif y < int(height - 1):
-				current_tile = current_biome.tile_atlas.dirt
-			else:
-				current_tile = current_biome.tile_atlas.grass
-
-			if current_biome.cave_noise_image.get_pixel(x, y).r > 0.5 or not current_biome.generate_caves:
-				place_tile(current_tile, x, y)
-
-			if y > int(height - 1):
-				if randi_range(0, current_biome.tree_percent_chance) == 1 and world_tiles.has(Vector2i(x, y)):
-					place_tree(current_biome, x, y)
-				elif current_biome.tile_atlas.addons != null and randi_range(0, current_biome.addon_percent_chance) == 1 and world_tiles.has(Vector2i(x, y)):
-					place_tile(current_biome.tile_atlas.addons, x, y + 1)
