5b7f7c109c2164bec72ce633066c217a2b7e2f26
Souvlaki42 <ilias@moulas.dev>
2025-12-01 22:02:22 +0200

    feat: starting working on player controller

---
+++ scripts/player.gd
@@ -0,0 +1,24 @@
+class_name Player extends CharacterBody2D
+
+@export var move_speed: float = 300.0
+@export var jump_velocity: float = -400.0
+@export var sprite_root: Sprite2D
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var direction: float = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * move_speed
+		if direction > 0:
+			sprite_root.scale.x = -1
+		if direction < 0:
+			sprite_root.scale.x = 1
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+
+	move_and_slide()
