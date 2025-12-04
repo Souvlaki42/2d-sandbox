adeb2f5c96524f3e3d8aa633eae4eb5f51d2afac
Souvlaki42 <ilias@moulas.dev>
2025-12-04 02:18:22 +0200

    feat: add foreground and background layers, tile collisions and world scene

--- scripts/tile_class.gd
+++ scripts/tile_class.gd
@@ -3,3 +3,4 @@ class_name Tile extends Resource
 @export var tile_name: StringName
 @export var source_id: int = 1
 @export var atlas_coords: Array[Vector2i] = [Vector2i.ZERO]
+@export var is_background: bool = false
